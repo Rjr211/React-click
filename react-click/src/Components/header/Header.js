753e581a0ac7e2dee1140d6de896a80a8fe4bd28
Rob <rjr211@scarletmail.rutgers.edu>
2018-10-11 16:23:07 -0400

    css

--- react-click/src/Components/header/Header.js
+++ react-click/src/Components/header/Header.js
@@ -1,13 +1,17 @@
 import React from "react";
 import "./Header.css";
 
-const Header = props => (
-  <div className="header">
-    <div className="title">{props.children}</div>
-    <div className="scores">
+
+const Header = props => ( 
+<nav className="black">
+<div className="nav-wrapper">
+<div className="material-icons">mouse</div>
+<div className="title">{props.children}</div>
+<div className="scores right">
       Score: {props.score} Highscore: {props.highscore}
     </div>
-  </div>
+</div>
+</nav>
 );
 
 export default Header;
